046f69e44d545ac47d4b4473164c6b305605c5b9
hagerElsehaimy <hager.m.elsehaimy@gmail.com>
2021-01-16 00:06:39 +0200

    add vcn, internetgateway and natgateway

---
+++ tf-provider/outputs.tf
@@ -0,0 +1,4 @@
+output "vcn"{
+    value = module.vcn.vcn_id
+    description = "vcn id"
+}
---
+++ tf-provider/variables.tf
@@ -0,0 +1,17 @@
+variable "tenancy_ocid" {
+  default = "ocid1.tenancy.oc1..aaaaaaaadus45gcw2hf7r6a5wfjegzovazmxqemryxhto5phlt2gpalpcahq"
+}
+
+variable "user_ocid" {
+  default = "ocid1.user.oc1..aaaaaaaavurwybmsv4mhuo656rnfhisho6oqkn6yim5xfw4aw7it3d2l5hda"
+}
+
+variable "fingerprint" {
+  default = "49:42:ba:b3:86:6c:ad:1c:21:a7:b5:65:c0:7c:9c:f0"
+}
+variable "region_identifier" {
+  default = "me-jeddah-1"
+}
+variable "private_key_path"{
+    default = "/Users/hager/.oci/oci.pem"
+}
