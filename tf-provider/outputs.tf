6891a1b8a8057fd2ab0053c12031c16bea633027
hagerElsehaimy <hager.m.elsehaimy@gmail.com>
2021-01-16 17:04:39 +0200

    get all required outputs from vcn

--- tf-provider/outputs.tf
+++ tf-provider/outputs.tf
@@ -1,4 +1,20 @@
-output "vcn"{
+output "vcn_id"{
     value = module.vcn.vcn_id
     description = "vcn id"
+}
+output "vcn_ig_rt"{
+    value = module.vcn.ig_route_id
+    description = "internet gateway route table id"
+}
+output "vcn_ig"{
+    value = module.vcn.internet_gateway_id
+    description = "internet gateway id"
+}
+output "vcn_nat"{
+    value = module.vcn.nat_gateway_id
+    description = "nat gateway id"
+}
+output "vcn_nat_rt"{
+    value = module.vcn.nat_route_id
+    description = "nat gateway route table id"
 }
