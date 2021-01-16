08794ebb9642aeb3078b9b09d1ee7723979b6fac
hagerElsehaimy <hager.m.elsehaimy@gmail.com>
2021-01-16 21:20:21 +0200

    output security group id

--- tf-provider/outputs.tf
+++ tf-provider/outputs.tf
@@ -18,3 +18,15 @@ output "vcn_nat_rt"{
     value = module.vcn.nat_route_id
     description = "nat gateway route table id"
 }
+output "public_subnet_A"{
+    value = oci_core_subnet.vcn-public-subnetA.id
+    description = "LB Tier"
+}
+output "public_subnet_B"{
+    value = oci_core_subnet.vcn-public-subnetB.id
+    description = "webserver Tier"
+}
+output "private_subnet"{
+    value = oci_core_subnet.vcn-private-subnet.id
+    description = "DB Tier"
+}
