6543648654406a7ee35347ad0862a0bf4632bd7b
KarimEl-Dweky <helsehai@HELSEHAI-T490.eg.oracle.com>
2021-10-13 16:18:59 +0200

    fix bash script

--- apache_init.sh
+++ apache_init.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
-sudo firewall-cmd --add-service=http --permanent
-sudo firewall-cmd --reload
 sudo yum install -y nginx
 sudo systemctl start nginx
+
+# sudo firewall-cmd --add-service=http --permanent
+# sudo firewall-cmd --reload
