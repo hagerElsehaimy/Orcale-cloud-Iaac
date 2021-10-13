357701b7eb43f495b54165c078bdccafd50aab10
KarimEl-Dweky <helsehai@HELSEHAI-T490.eg.oracle.com>
2021-10-13 20:08:42 +0200

    set connection to remote exec

--- webserver.tf
+++ webserver.tf
@@ -35,20 +35,19 @@ resource "oci_core_instance" "web_server_A" {
   provisioner "file" {
     source      = "apache_init.sh"
     destination = "/home/opc/apache_init.sh"
-  connection {
-    type     = "ssh"
-    user     = "opc"
-   # password = "${var.root_password}"
-    private_key = file("./private.key")
-    host     = oci_core_instance.web_server_A.public_ip
-    }
   }
    provisioner "remote-exec" {
     inline = [
       "chmod +x /tmp/apache_init.sh",
       "./apache_init.sh",
     ]
-    
+     connection {
+    type     = "ssh"
+    user     = "opc"
+   # password = "${var.root_password}"
+    private_key = file("private.key")
+    host     = oci_core_instance.web_server_A.public_ip
+    }
   }
 }
 
